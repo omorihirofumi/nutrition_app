654394eb61bdacfee2f3d7cd43df702bda478aaf
omorihirofumi <kanazawabunko827@icloud.com>
2020-03-22 11:27:42 +0900

    バリデーション追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  validates :birthday, presence: true
+  validates :sex, presence: true
+  validates :height, presence: true
+  validates :weight, presence: true
 end
