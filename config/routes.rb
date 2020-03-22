9cd1392dd3067041f253470d65c87145b27dbb5a
omorihirofumi <kanazawabunko827@icloud.com>
2020-03-22 10:11:04 +0900

    豆知識追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#top'
-  # get 'users/index'
-  
-  # get 'calendar/index'
-  
   resources :home, as: :homes, only: [:new, :create] do
     collection do
       get :top
+      get :tribia1
+      get :tribia2
+      get :tribia3
+      get :tribia4
     end
   end
   resources :profile, as: :profiles, only: [:new, :create, :edit, :update]
-  # resources :blogs
-
-  
-  # get 'home/top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
